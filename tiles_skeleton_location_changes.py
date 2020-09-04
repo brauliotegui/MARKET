66eacaeb413fc8a09463573b3b582c49706eabd0
maxkoeppel <m_koeppel@web.de>
2020-09-04 11:17:14 +0200

    updated version, new layout, ghost and customer

--- tiles_skeleton_location_changes.py
+++ tiles_skeleton_location_changes.py
@@ -24,7 +24,7 @@ MARKET = """
 """.strip()
 
 
-class TiledMap:             # constructor for the map, connecting the image to the code, builds the objects from the class; here I understand it only on a higher level, but I believe for this weeks task this is enough
+class TiledMap:
 
     def __init__(self, layout, tiles):
         self.tiles = tiles
@@ -34,11 +34,31 @@ class TiledMap:             # constructor for the map, connecting the image to t
         self.image = np.zeros((self.ysize * TILE_SIZE, self.xsize * TILE_SIZE, 3), dtype=np.uint8)
         self.prepare_map()
 
-    def get_tile_bitmap(self, char): # general format of tiles: [y-coord, x-coord, color-tuple]
-        if char == '#':             #  if character is '#' in MARKET-> slice here, turning hash-symbols into blue squares
-            return self.tiles[0:32, 0:32, :]
-        elif char == 'b':           # gives shop-able items, like bananas or else, written into MARKET at the top
-            return self.tiles[0:32, -1*TILE_SIZE:, :]
+    def get_tile_bitmap(self, char):
+        if char == '#':
+            return self.tiles[0:32, 0:32, :]  #y, x, z
+        elif char == 'b':
+            return self.tiles[0:32, 128:160, :]
+        elif char == 'd':
+            return self.tiles[64:96, 128:160, :]
+        elif char == 'w':
+            return self.tiles[96:128, 128:160, :]
+        elif char == 'a':
+            return self.tiles[96:128, 160:192, :]
+        elif char == 'q':
+            return self.tiles[32:64, 128:160, :]
+        elif char == 'p':
+            return self.tiles[64:96, 192:224, :]
+        elif char == 'x':
+            return self.tiles[128:160, 128:160, :]
+        elif char == 'y':
+            return self.tiles[192:224, 96:128, :]
+        elif char == 'z':
+            return self.tiles[160:192, 96:128, :]
+        elif char == 'm':
+            return self.tiles[96:128, 224:256, :]
+        elif char == 's':
+            return self.tiles[32:64, 0:32, :]
         else:
             return self.tiles[32:64, 64:96, :]
 
@@ -105,24 +125,45 @@ class Customer:
     def __repr__(self):                     # returns where the customer is drwan, if no errors occur
         return f'''is in location {self.current_location}'''
 
+class Ghost:
+
+    def __init__(self, tmap, image, x, y): # how its position on the map cn be definedwhen the calss is instantiated
+         self.tmap = tmap
+         self.image = image
+         self.x = x
+         self.y = y
+
+    def draw(self, frame):              # puts the ghost-object onto the map
+        xpos = OFS + self.x * TILE_SIZE
+        ypos = OFS + self.y * TILE_SIZE
+        frame[ypos:ypos+TILE_SIZE, xpos:xpos+TILE_SIZE] = self.image
+
+    def move(self, direction):
+        newx = self.x
+        newy = self.y
+        newy += random.randint(-1, 1)
+        newx += random.randint(-1, 1)
+
+        if self.tmap.contents[newy][newx] != '#':    #if statement allows customers to move only on dots, not on # in the MARKET
+            self.x = newx                             # changed inorder not to be able to moveon #, but yet onto groceries
+            self.y = newy
 
 background = np.zeros((700, 1000, 3), np.uint8)  # not sure what this does
-tiles = cv2.imread('tiles.png')                 # calls the empty supermarket image (which has to be in the same folder as the script)
+tiles = cv2.imread('tiles2.png')                 # calls the empty supermarket image (which has to be in the same folder as the script)
 
 # takes the position and thereby one of the different images from  the tiles.png
 
 
 customer_image = tiles[-2*TILE_SIZE:-1*TILE_SIZE,:1*TILE_SIZE]  # this one gives a ghost
+customer_image2 = tiles[3*TILE_SIZE:4*TILE_SIZE,1*TILE_SIZE:2*TILE_SIZE]
 
 possible_locations = ['dairy', 'drinks', 'fruits', 'spices']
 
 tmap = TiledMap(MARKET, tiles)
 
-c = Customer(tmap,customer_image, 15,10,'spices')
+c = Customer(tmap,customer_image2, 15,10,'spices')
+g = Ghost(tmap,customer_image, 12,9)
 
- # a simple way of increasing the number of (identical looking) customers, to include only one, the shorter version of bunch is used
-#bunch = [Customer(tmap, customer_image, 15, 10)]
-#bunch = [Customer(tmap, customer_image, 15-np.random.randint(3), 10np.random.randint(3)) for c in range(10)]
 
 # infinite loop to refresh the frame with supermarket and customers on
 while True:
@@ -131,8 +172,9 @@ while True:
 
     #c.draw(frame)
     c.change_location()
-    # for c in bunch:
-    #    c.draw(frame)
+
+    g.move(frame)
+    g.draw(frame)
 
     cv2.imshow('frame', frame)
 
@@ -153,12 +195,3 @@ while True:
     # print(new_location)
 
 cv2.destroyAllWindows()
-
-
-# sniplets that might  be useful for implementing movement based on locations and transition probabilites, far from working
-# Location_class filling with customers
-#
-# #Option 1:
-#         if self.new_state == 'spices':
-#             newx = self.spices[np.random.randint(ailse_start,aisle_end)]
-#             newy = self.spices[np.random.randint(ailse_start,aisle_end)]
